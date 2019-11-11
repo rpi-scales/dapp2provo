eb009212602e5124f21113c6589ffebbaeb7e701
Dan K <dkazenoff@gmail.com>
2019-11-11 00:58:58 -0500

    adding index.js and index.html, run instructions

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
-### Dependencies Required
-1. Node (check with ```node -v```)
+### PROV-O to Neo4j transpiler using a file drag-and-drop. 
+It runs on localhost:9966, and works by pasting in a text file of PROV syntax (assuming the syntax is correct). "infile.txt" was taken from https://www.w3.org/TR/prov-o/, example 1.
+- 1 terminal window running your neo4j shell:
+`bin/neo4j console` inside your installed neo4j directory
+- 1 terminal window running your local web server:
+`npm start`
+
+To view the results of your Provenance graph, navigate to localhost:7474 and run 
+`MATCH (n) RETURN n`
+If it's your first time launching neo4j on your computer, you'll need to generate a fresh password.
+
+#### To run this, you'll need:
+1. Node (check with ```node -v```) and npm (`npm install`)
+2. Neo4j Server installed somewhere on your system for running the graph server (https://neo4j.com/download-center/#community). This version was created with v3.5.3
+3. Neo4j JavaScript driver (v1.7 is the stablerelease) ```npm install neo4j-driver```
+4. XML-HTTP-REQUEST driver `npm install --save xmlhttprequest`
+
+
+<!-- 1. Node (check with ```node -v```)
 2. Truffle (```npm install -g truffle```)
 3. Ganache
 4. Metamask (Chrome Browser Extension)
 5. Solidity Syntax Highlighter in IDE of your choice.
 --------
 6. Neo4j Server installed (separately somewhere on your system) for running the graph server (https://neo4j.com/download-center/#community)
-7. Neo4j JavaScript driver (```npm install neo4j-driver```) is already installed in this repo.
+7. Neo4j JavaScript driver (```npm install neo4j-driver```) is already installed in this repo. -->
 <!-- 
 ### Backend - Run Steps
 1. Have Ganache running / launched 
--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 const neo4j = require('neo4j-driver').v1;
-const driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "neo4j123"));
+const driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "neo4j"));  //Or whatever your neo4j credentials are
 const session = driver.session()
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-const fs = require('fs') 
-let allText = '' 
-let bigarray = []
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                allText = rawFile.responseText;
-                separateChunks(allText);
-                // bigarray.push[allText]
-                // console.log(allText);
-            }
-        }
-    }
-    rawFile.send(null);
-}
+const input = document.querySelector('input[type="file"]')
+input.addEventListener('change', function (e) {
+	console.log(input.files)
+	const reader = new FileReader()
+	reader.onload = function () {
+		const text_blob = reader.result
+		separateChunks(text_blob);
+		// const lines = reader.result.split('\n.').map(function (line) {
+		// 	return line.split(';')
+		// })
+		console.log(lines)
+		console.log(lines.size)
+	}
+	reader.readAsText(input.files[0])
+}, false) 
 function separateChunks(text_blob){
     var chunks = text_blob.split('\n.') //chunks is now an array of all the strings inside the chunk
     let numact= 0;
@@ -47,7 +40,6 @@ function separateChunks(text_blob){
           current_name=lines[j-1].trim()
           // console.log(chunks[i])
           // console.log(current_name)
-          // ++numact;
           // console.log(current_chunk);
           let options = {
             label: current_chunk,
@@ -78,14 +70,14 @@ function separateChunks(text_blob){
         }
 //Activity Functions
         if (current_chunk === "Activity") {
-            // console.log("Jar")
+            console.log("Jar")
             let trimmed_line = lines[j].replace(/\s+/g,' ').trim();
             console.log("trimmed_line: ", trimmed_line)
             let trim_line_array = trimmed_line.split(' ')
             console.log("trim_line_array: ", trim_line_array)
             let part1 = trim_line_array[0]
             console.log("part1: ", part1)
-            let part2 = trim_line_array[1]
+            let part2 = trim_line_array[1].slice(0,-1)
             console.log("part2 ", part2)
 
             if (part1.includes("startedAtTime")) {
@@ -96,7 +88,6 @@ function separateChunks(text_blob){
                   value: part2
                 }
                 set_props(options)
-              // match (a:current_chunk {name: $current_name}) set a.startedAtTime = "second part of line j"
             }
             
             else if (part1.includes("endedAtTime")) {
@@ -110,7 +101,6 @@ function separateChunks(text_blob){
             }
 
             else if (part1.includes("wasAssociatedWith")){
-              // match (a:current_chunk {name: $current_name}) merge (a)-[wasAssociatedWith]->(b:Agent {name of second part of line j)"
               let options = {
                   label: current_chunk,
                   name: current_name.trim(),
@@ -123,7 +113,6 @@ function separateChunks(text_blob){
             }
 
             else if (part1.includes("used")) {
-              // match (a:current_chunk {name: $current_name}) merge (a)-[used]->(b:Entity {with name of second part of line j)"
                let options = {
                   label: current_chunk,
                   name: current_name,
@@ -143,16 +132,75 @@ function separateChunks(text_blob){
                   value: part2
                 }
                 create_rels(options)
-              // match (a:current_chunk {name: $current_name}) merge (a)-[wasInformedBy]->(b:Activity {with name of second part of line j)"
             }
         
         } //close out "if Activity"
 
-      }
-    }
-    // console.log('Number of Activities:', numact);
-    
+        if (current_chunk === "Agent") {
+            let trimmed_line = lines[j].replace(/\s+/g,' ').trim();   // remove extra spaces between lines
+            console.log("trimmed_line: ", trimmed_line)
+            let trim_line_array = trimmed_line.split(' ')
+            console.log("trim_line_array: ", trim_line_array)
+            let part1 = trim_line_array[0]
+            console.log("part1: ", part1)
+            let part2 = trim_line_array[1].slice(0,-1)              //trim off ";" so isn't included in node props.
+            console.log("part2 ", part2)
+            if (part1.includes("actedOnBehalfOf")) {
+                let options = {
+                  label: current_chunk,
+                  name: current_name,
+                  relationship: "actedOnBehalfOf",
+                  label2: "Agent",
+                  value: part2
+                }
+                create_rels(options)
+              }
 
+        }   //close out  "IF Agent"
+        if (current_chunk === "Entity") {
+            let trimmed_line = lines[j].replace(/\s+/g,' ').trim();   // remove extra spaces between lines
+            // console.log("trimmed_line: ", trimmed_line)
+            let trim_line_array = trimmed_line.split(' ')
+            // console.log("trim_line_array: ", trim_line_array)
+            let part1 = trim_line_array[0]
+            // console.log("part1: ", part1)
+            let part2 = trim_line_array[1].slice(0,-1)  //to remove the ";'s" at end of every line
+            // console.log("part2 ", part2)
+            if (part1.includes("wasAttributedTo")) {
+                let options = {
+                  label: current_chunk,
+                  name: current_name,
+                  relationship: "wasAttributedTo",
+                  label2: "Agent",
+                  value: part2
+                }
+                create_rels(options)
+            }
+
+            if (part1.includes("wasGeneratedBy")){
+                let options = {
+                  label: current_chunk,
+                  name: current_name,
+                  relationship: "wasGeneratedBy",
+                  label2: "Activity",
+                  value: part2
+                }
+                create_rels(options)
+            }
+        
+            if (part1.includes("wasDerivedFrom")) {
+                let options = {
+                  label: current_chunk,
+                  name: current_name,
+                  relationship: "wasDerivedFrom",
+                  label2: "Entity",
+                  value: part2
+                }
+                create_rels(options)
+            }
+          }   //close out "if ENTITY"
+        }
+      }    
 } 
 
 //end sepchunk function
@@ -212,61 +260,5 @@ function set_props(options){
 
 
 
-
-
-readTextFile('file:///Users/dan/Projects/bigbase/n4jprov/infile.txt')
+// readTextFile('file:///Users/dan/Projects/bigbase/n4jprov/infile.txt')
 // console.log(allText)
-
-
-
-// const resultPromise = session.writeTransaction(tx =>
-//   tx.run(
-//     'CREATE (a:test) SET a.message = $message RETURN a.message + ", from node " + id(a)',
-//     { message: 'hello, world' }
-//   )
-// )
-
-// resultPromise.then(result => {
-//   session.close()
-
-//   const singleRecord = result.records[0]
-//   const greeting = singleRecord.get(0)
-
-//   console.log(greeting)
-
-//   // on application exit:
-//   driver.close()
-
-
-
-
-
-
-// function build_nodes(options){
-//     console.log("hi")
-//     console.log('first call', options)
-//     const resultPromise = session.writeTransaction(tx =>
-//       tx.run(
-//         `MERGE (a:${options.label} {name: $name})`,
-//         { 
-//           label: options.label,
-//           name: options.name, 
-//         }
-//       )
-//     )
-//     console.log('2nd part', options)
-//     resultPromise.then(result => {
-//       session.close()
-//       // console.log(result.records[0].singleRecord.get(0))
-//       const singleRecord = result.records[0]
-//       const greeting = singleRecord.get(0)
-
-//       // console.log(greeting)
-
-//       // on application exit:
-//       driver.close()
-//       return 0
-//     })
-//     return 1
-// }
-// })
