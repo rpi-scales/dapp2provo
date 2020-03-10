88adebc2252cc850081b1444293480c55a19151b
Dan K <dkazenoff@gmail.com>
2020-03-10 00:46:44 -0400

    .gitignorefixed

--- config/default.js
+++ config/default.js
@@ -2,7 +2,7 @@ const creds = {
     database: {
         uri: "bolt://0.0.0.0:7687",
         username: "neo4j",
-        password: "rpiurp1824"
+        // password: "rpiurp1824"
     },
     etherscan: {
         api_key: "RNSA88B918VZTTCR3662NYRX5MQXTM8Q1V"
